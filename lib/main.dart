df8ce417e44981b306467db0841ad792852f20e8
NaHid Hasan Meraj <nahid.meraj765@gmail.com>
2025-02-11 14:42:49 +0600

    start commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_quiz_app/start_screen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: StartScreen(),
+      ),
+    ),
+  );
+}
