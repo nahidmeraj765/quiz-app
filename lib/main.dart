25a06df81636e22825b66e39e59d73a41ea52a37
NaHid Hasan Meraj <nahid.meraj765@gmail.com>
2025-02-11 19:19:36 +0600

    start screen design commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,20 @@ import 'package:flutter_application_quiz_app/start_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: StartScreen(),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Colors.deepPurple,
+              Colors.blue,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
       ),
     ),
   );
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,6 +5,24 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return const Text("Welcome");
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset("assets/images/quiz-logo.png", width: 300),
+          const SizedBox(height: 80),
+          const Text(
+            "Learn Flutter the Fun Way!",
+            style: TextStyle(color: Colors.white, fontSize: 28),
+          ),
+          const SizedBox(height: 40),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            child: const Text("Start Quiz"),
+          ),
+        ],
+      ),
+    );
   }
 }
