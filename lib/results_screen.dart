b39d3a66a4409e06da4199d02059772f5da7dce0
NaHid Hasan Meraj <nahid.meraj765@gmail.com>
2025-05-03 23:38:53 +0600

    combining maps and loops

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_quiz_app/data/questions.dart';
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key,required this.chooseAnswers});
+  const ResultsScreen({super.key, required this.chooseAnswers});
 
   final List<String> chooseAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chooseAnswers.length; i++) {
+      summary.add({
+        'questions_index' : i,
+        'questions' : questions[i].questions,
+        'correct_answers' : questions[i].answers[0],
+        'user_answers' : chooseAnswers[i],
+      });
+    }
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
